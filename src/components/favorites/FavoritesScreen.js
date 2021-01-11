2867d0d86a7d7ede36b648f68736502780f5bfd6
unknown <daniellopezj0327@gmail.com>
2021-01-11 10:51:34 -0500

    changes

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import colors from '../../res/Colors'
 import FavoritesEmptyState from './FavoritesEmptyState'
+import { getAllKeys, getAll } from '../../libs/storage'
+import CoinsItem from '../coins/CoinsItem'
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({ navigation }) => {
+
+  const [favorites, setFavorites] = useState([])
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getData();
+    });
+    return unsubscribe;
+  }, [navigation])
+
+  const getData = async () => {
+    const allKeys = await getAllKeys()
+    const keys = allKeys.filter((key) => key.includes('favorite='))
+    const favs = await getAll(keys)
+    const allFavorites = favs.map((fav) => JSON.parse(fav[1]))
+    setFavorites(allFavorites)
+  }
+
+  handlePress = (coinDetail) => {
+    navigation.navigate('CoinDetail', { coinDetail })
+  }
+
   return (
     <View style={styles.container}>
-      <FavoritesEmptyState />
+      {
+        favorites.length === 0
+          ? <FavoritesEmptyState />
+          : <FlatList
+            data={favorites}
+            renderItem={({ item }) =>
+              <CoinsItem
+                item={item}
+                onPress={() => handlePress(item)}
+              />}
+          />
+      }
     </View>
   )
 }
